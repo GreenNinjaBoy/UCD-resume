9fb32e4d1db99f68be07b144c5e3ab3967b238f3
J.Connell1990 <68825060+GreenNinjaBoy@users.noreply.github.com>
2023-12-12 19:08:54 +0000

    add more to githib-information.js

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -17,15 +17,15 @@ function userInformationHTML(user) {
 
 function fetchGitHubInformation(event) {
 
-    var username = $("gh-username").val();
+    var username = $("#gh-username").val();
     if (!username) {
-        $("gh-user-data").htnl(`<h2>Please enter a GitHub username</h2>`);
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
 
-    $("gh-user-data").html(
+    $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading..." /> 
+            <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
     $.when(
@@ -33,10 +33,12 @@ function fetchGitHubInformation(event) {
     ).then(
         function (response) {
             var userData = response;
-            $("gh-user-data").html(userInformationHTML(userData));
-        }, function (errorResponse) {
+            $("#gh-user-data").html(userInformationHTML(userData));
+        },
+        function (errorResponse) {
             if (errorResponse.status === 404) {
-                $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
